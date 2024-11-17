211564e2badab39d7260fbd280635c11dd794614
TharaniDharan10 <TharaniDharan.S@outlook.com>
2024-11-17 15:04:56 +0530

    Made tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -12,3 +9,8 @@ function App() {
 }
 
 export default App;
+
+// cli imports we have made
+// npm install -D tailwindcss postcss autoprefixer    (command to install tailwind) (-D -> adds these in dev dependencies in package.json )
+// npm install lucide-react  (library for icons)
+// npx tailwindcss init -p   (to initialise tailwind.config.js & postcss.config.js)
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], //this tells that the tailwind should be applied to these files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
